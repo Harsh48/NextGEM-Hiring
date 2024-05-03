f0cbac751a7a812198dbed4d3e6919e0a3b71cf4
HarshEb <harsh.techilia@gmail.com>
2024-05-04 05:20:02 +0530

    fix: adding enum values in type

--- senior-backend/apps/scraper/config/units.js
+++ senior-backend/apps/scraper/config/units.js
@@ -2,31 +2,23 @@ const { default: axios } = require('axios');
 const request = require('axios');
 
 async function getWethPriceUsd() {
-  // Replace with your preferred API endpoint and access key if needed
   const baseUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=exactly-weth&vs_currencies=usd';
-  const url = baseUrl;
-
   try {
-    const {data} = await axios.get(url);
-    console.log(data)
+    const {data} = await axios.get(baseUrl);
     return data['exactly-weth'].usd;
 
   } catch (error) {
-    throw error; // Re-throw for proper error handling
+    throw error; 
   }
 }
  
  async function getGemaiPriceEth() {
-    // Replace with your preferred API endpoint and access key if needed
-    const apiKey = ''
     const baseUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=the-next-gem-ai&vs_currencies=eth';
-    const url = baseUrl;
     try {
-        const {data} = await axios.get(url);
-        console.log(data)
+        const {data} = await axios.get(baseUrl);
         return data['the-next-gem-ai'].eth;
       } catch (error) {
-        throw error; // Re-throw for proper error handling
+        throw error; 
       }
 }
 
--- senior-backend/apps/scraper/models/transaction.model.js
+++ senior-backend/apps/scraper/models/transaction.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
   const transactionSchema = new mongoose.Schema({
-    type: { type: String, required: true },
+    type: { type: String, required: true, enum: ['buy', 'sell'] },
     txid: { type: String, required: true },
     value_usd: { type: Number, required: true },
     value_weth: { type: Number, required: true },
